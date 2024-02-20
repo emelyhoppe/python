ac3c32451f2d96d1668a160555cabf0b2e99105a
emelyhoppe <128698341+emelyhoppe@users.noreply.github.com>
2024-02-20 09:39:32 -0300

    Add files via upload

---
+++ teste2.py
@@ -0,0 +1,16 @@
+#nome = input("Escreva seu nome")
+#try: 
+    #idade = int(input("Escreva sua idade"))
+#except ValueError:
+    #print("Você não digitou um número")
+#print (f"Olá, {nome}, você tem {idade} anos.")
+
+try:
+    n1 = float(input("digite um número"))
+    n2 = float(input("digite mais um número"))
+except ValueError:
+    print("Tente novamente")
+
+soma = n1 + n2
+
+print (f"O número {n1} mais o número {n2} é igual a: {soma}.")
