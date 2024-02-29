49d2a1b12df8186ef1ba89bec49633e6045c51d5
emelyhoppe <128698341+emelyhoppe@users.noreply.github.com>
2024-02-29 08:39:30 -0300

    aula 1 de forca (29/02)

---
+++ jogodaforca.py
@@ -0,0 +1,23 @@
+palavra_secreta = "uva"
+letras_acertadas = ["_","_","_"]
+tentativas = 5
+
+while tentativas > 0 and "_" in letras_acertadas:
+    palpite = input("Digite uma letra:").lower()
+    
+    if palpite in palavra_secreta: 
+        index = 0
+        for letra in palavra_secreta:
+            if palpite == letra: 
+                letras_acertadas[index] = letra
+            index += 1
+    else: 
+        tentativas -= 1
+        print(f"Você tem {tentativas} restantes.")
+        
+    print(" ".join(letras_acertadas))
+        
+if "_" not in letras_acertadas:
+    print ("Parabéns você ganhou!")
+else: 
+    print (f"Que pena, você perdeu a palavra era: {palavra_secreta}.")
