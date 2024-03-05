53ed19ba2c2bdd84ff21810c11d8ba65ed16ff2a
emelyhoppe <128698341+emelyhoppe@users.noreply.github.com>
2024-03-05 08:26:44 -0300

    atividade que junta todos os jogos

---
+++ jogos.py
@@ -0,0 +1,20 @@
+import jogodaforca
+import adivinhacao
+
+def escolha_jogo():
+    print("************************")
+    print("****Escolha seu jogo****")
+    print("************************")
+    print("(1)Forca, (2)Adivinhação")
+
+    jogo = int(input("Qual jogo você prefere?"))
+
+    if (jogo == 1):
+        print("Jogo da Forca")
+        jogodaforca.jogar
+    elif (jogo == 2):
+        print("Jogo da Adivinhação")
+        adivinhacao.jogar_adivinhacao
+
+if (__name__ == "__main__"):
+    escolha_jogo()
