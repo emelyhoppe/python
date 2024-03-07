537898b197fb7725d392ebd7533bcdb4ce7efab3
emelyhoppe <128698341+emelyhoppe@users.noreply.github.com>
2024-03-07 12:06:28 -0300

    Add files via upload

---
+++ att1.py
@@ -0,0 +1,9 @@
+nota1 = int(input("Digite uma nota"))
+nota2 = int(input("Digite mais uma nota"))
+nota3 = int(input("Digite e mais uma nota"))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print (f"A média de suas notas é de {media}")
+    
+
